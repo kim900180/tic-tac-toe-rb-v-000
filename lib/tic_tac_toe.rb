3bee7cb5029c3ca6ba01bc4f788b8422b50561ef
Jae Yong Kim <kim900180@gmail.com>
2018-07-24 05:54:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -130,7 +130,7 @@ def play(board)
       if turn_count(board) < 2
         turn_count(board)
         turn(board)
-        over?(board)
+        draw?(board)
         result = false
       elsif turn_count(board) > 1
         turn_count(board)
